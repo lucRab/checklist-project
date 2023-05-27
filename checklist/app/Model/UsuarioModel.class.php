1bbc192c1d4f82ceed06aeef3461ddc6d00a7892
lucRab <99261962+lucRab@users.noreply.github.com>
2023-05-27 17:41:29 -0300

    Add files via upload

---
+++ checklist/app/Model/UsuarioModel.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Model;
+/**
+ *  Classse model responsavel pelos dados do usuario
+ * @author Lucas Rabelo <lucasrabelo186@gmail.com>
+ * @version ${1:1.0.0
+ */
+
+ class UsuarioModel{
+
+    
+ }
