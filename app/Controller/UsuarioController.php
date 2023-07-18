151a0bcb8a3a44b7803a3f259170e4acf2e24402
Lucas Rabelo <99261962+lucRab@users.noreply.github.com>
2023-07-18 00:10:06 -0300

    css login page

---
+++ app/Controller/UsuarioController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\controller;
+use Core\conexao\Conexao;
+use PDO;
+class UsuarioController {
+
+
+}
--- app/View/loginPage.php
+++ app/View/loginPage.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html>
 <html>
     <header>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <title>Login</title>
     </header>
     <body>
-        <div class = "container">
+        <div class = "container text-center">
             <h1>Sitema de Checklist</h1>
-            <div class = "login">
+            <div class="card mx-auto" style="width: 30rem;">
                 <form>
-                    <h1>Olá!</h1>
-                    <p>
-                        <label for="username">Usuario</label><br>
-                        <input id="username" type="text" placeholder="Informe seu usuario"/>
-                    </p>
-                    <p>
-                        <label for="senha">Senha</label><br>
-                        <input id="senha" type="text" placeholder="Informe sua senha"/>
-                    </p>
-                    <p>
-                        <input type="submit" class="btn-sub" value="Logar"/>
-                    </p>
+                    <div class="card-body ">
+                        <h2 class="card-title ">Olá!</h2>
+                        <div class="p-3">
+                            <label for="username" class="form-label" >Usuario</label><br>
+                            <input id="username" type="text" class="form-control" placeholder="Informe seu usuario"/>
+                        </div>
+                        <div class=" p-3">
+                            <label for="senha" class="form-label">Senha</label><br>
+                            <input id="senha" type="text" class="form-control" placeholder="Informe sua senha"/>
+                        </div>
+                        <div class="p-">
+                            <button type="submit" class="btn btn-primary">Logar</button>
+                        </div>
+                        <p class="card-link">
+                            Sem conta?
+                            <a href="login/cadastro">Cadastre-se</a>
+                        </p>
+                    </div>
                 </form>
             </div>
         </div>
