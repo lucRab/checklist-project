491447891dbd0313d996beff351d4c3f35467fb4
Lucas Rabelo <99261962+lucRab@users.noreply.github.com>
2023-08-18 15:35:12 -0300

    home atualizações

--- app/View/main.php
+++ app/View/main.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="app/View/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="app/View/assets/css/templatemo-lugx-gaming.css">
+    <link rel="stylesheet" href="app/View/assets/css/owl.css">
+    <link rel="stylesheet" href="app/View/assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 589 lugx gaming
+
+https://templatemo.com/tm-589-lugx-gaming
+
+-->
 </head>
 <body>
     <?=$this->section('content')?>
